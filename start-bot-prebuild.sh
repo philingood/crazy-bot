9ae446ad35fa13cbaf100602342627187b6a48a6
philingood <98781376+philingood@users.noreply.github.com>
2024-03-12 14:30:09 +0300

    Add script for pull and up prebuild docker img

--- start-bot-prebuild.sh
+++ start-bot-prebuild.sh
@@ -5,4 +5,14 @@ mkdir crazy-vpn-bot && cd crazy-vpn-bot
 vim .env
 
 docker pull philingood/crazy-vpn-bot
-docker run -d --restart always philingood/crazy-vpn-bot
+
+docker run \
+    --name crazy-vpn-bot \
+    --env-file .env \
+    --restart=always \
+    -d \
+    philingood/crazy-vpn-bot
+
+rm .env
+cd ..
+rm -rf crazy-vpn-bot
