90647eca9c7cf7ab9df4e301b55d5cd447333c7c
philingood <98781376+philingood@users.noreply.github.com>
2024-03-15 19:12:50 +0300

    Noob moves

--- dispatcher.py
+++ dispatcher.py
@@ -5,10 +5,11 @@ import config
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-if not config.BOT_TOKEN:
-    exit("No token provided")
+# if not config.BOT_TOKEN:
+#     exit("No token provided")
 
-bot = Bot(token=config.BOT_TOKEN, parse_mode="HTML")
+# bot = Bot(token=config.BOT_TOKEN, parse_mode="HTML")
+bot = Bot(token=config.BOT_TOKEN)
 dp = Dispatcher()
 
 # dp.include_routers(base_commands.router, payments.router)
--- handlers/payments.py
+++ handlers/payments.py
@@ -1,13 +1,17 @@
 from aiogram import Router, F
 from aiogram.filters import Command
-from aiogram.types import Message, LabeledPrice, PreCheckoutQuery
+from aiogram.types import Message, LabeledPrice, PreCheckoutQuery, InlineKeyboardButton, CallbackQuery
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 import config
 import dispatcher as dp
 
 router = Router()
 
+ADMIN_ID = config.ADMIN_ID
+
 # prices
-PRICE = LabeledPrice(label="Подписка на 1 месяц", amount=79 * 100)  # в копейках (руб)
+PRICE = LabeledPrice(label="Подписка на 1 месяц",
+                     amount=79 * 100)  # в копейках (руб)
 
 
 @router.message(Command("pay"))
@@ -16,19 +20,19 @@ async def cmd_pay(message: Message):
         await message.reply(
             "Тестовый платеж!!!\n  Для оплаты используйте данные тестовой карты: 1111 1111 1111 1026, 12/22, CVC 000.")
 
-    await dp.bot.send_invoice(message.chat.id,
-                              title="Подписка на бота",
-                              description="Активация подписки на бота на 1 месяц",
-                              provider_token=config.PAY_TOKEN,
-                              currency="rub",
-                              photo_url="https://hidemy.io/media/img/news/vpn-servisy-otkazali-roskomnadzoru.jpg",
-                              photo_width=391,
-                              photo_height=261,
-                              photo_size=88064,
-                              is_flexible=False,
-                              prices=[PRICE],
-                              start_parameter="one-month-subscription",
-                              payload="test-invoice-payload")
+        await dp.bot.send_invoice(message.chat.id,
+                                  title="Подписка на бота",
+                                  description="Активация подписки на бота на 1 месяц",
+                                  provider_token=config.PAY_TOKEN,
+                                  currency="rub",
+                                  photo_url="https://hidemy.io/media/img/news/vpn-servisy-otkazali-roskomnadzoru.jpg",
+                                  photo_width=391,
+                                  photo_height=261,
+                                  photo_size=88064,
+                                  is_flexible=False,
+                                  prices=[PRICE],
+                                  start_parameter="one-month-subscription",
+                                  payload="test-invoice-payload")
 
 
 # pre checkout  (must be answered in 10 seconds)
@@ -44,3 +48,27 @@ async def successful_payment(message: Message):
 
     await dp.bot.send_message(message.chat.id,
                               f"Платеж на сумму {message.successful_payment.total_amount / 100} {message.successful_payment.currency} прошел успешно!!!")
+
+
+@router.message(Command("order"))
+async def cmd_order(message: Message):
+    builder = InlineKeyboardBuilder()
+    builder.add(InlineKeyboardButton(
+        text="Оформить заказ",
+        callback_data="order")
+    )
+    await message.answer(
+        f"После нажатия на кнопку 'Оформить заказ' будет создана заявка. Все заявки обрабатываются вручную админом. После того как заявка будет обработана, я пришлю вам информацию для оплаты.",
+    )
+
+
+@router.callback_query(F.data == "order")
+async def callback_query_order(callback: CallbackQuery):
+    await dp.bot.send_message(
+        ADMIN_ID,
+        
+    )
+    await callback.message.answer(
+        f"Заказ на сумму {config.ORDER_PRICE / 100} {config.ORDER_CURRENCY}"
+
+    )
