687af14894c3bf3839bdbd592871f54c3523c5f7
philingood <98781376+philingood@users.noreply.github.com>
2024-05-30 15:27:19 +0300

    removed: docker stop container in gha

--- src/test/test_mode.py
+++ src/test/test_mode.py
@@ -7,4 +7,5 @@ async def notify_admin_test_mode(bot: Bot):
     if config.TEST_MODE:
         ADMIN_ID = int(config.ADMIN_ID)
         await bot.send_message(chat_id=ADMIN_ID, text="Бот запущен в тестовом режиме")
-        await stop_docker_container()
+        raise SystemExit(0)
+        # await stop_docker_container()
