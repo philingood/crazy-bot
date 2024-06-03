610133f5fe4d492b4d0d2bfda281d1161b612a18
philingood <98781376+philingood@users.noreply.github.com>
2024-06-03 10:58:44 +0300

    added: sending name of bot in test mode

--- src/utils/test_mode.py
+++ src/utils/test_mode.py
@@ -1,9 +1,19 @@
-import config
+import logging
+
 from aiogram import Bot
+
+import config
+
+logger = logging.getLogger(__name__)
 
 
 async def notify_admin_test_mode(bot: Bot):
     if config.TEST_MODE:
-        ADMIN_ID = int(config.ADMIN_ID)
-        await bot.send_message(chat_id=ADMIN_ID, text="Бот запущен в тестовом режиме")
+        if config.ADMIN_ID is None:
+            logger.error("ADMIN_ID not set in config.py")
+        else:
+            ADMIN_ID = int(config.ADMIN_ID)
+            await bot.send_message(
+                chat_id=ADMIN_ID, text="crazy-bot: Бот запущен в тестовом режиме"
+            )
         raise SystemExit(0)
