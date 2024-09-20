11cda788c5d026dcadb1f14b5e3a808fd6e75fb5
philingood <98781376+philingood@users.noreply.github.com>
2024-09-20 21:40:18 +0300

    added some base stufff

---
+++ src/db/db.py
@@ -0,0 +1,53 @@
+import sqlite3
+
+try:
+    from config import DATABASE_FILE
+except ImportError:
+    DATABASE_FILE = "/Users/booba/Downloads/x-ui.db"
+    pass
+
+
+def connect_to_db(db_path):
+    try:
+        conn = sqlite3.connect(db_path)
+        print("Подключение к базе данных прошло успешно")
+        return conn
+    except sqlite3.Error as e:
+        print(f"Ошибка подключения к базе данных: {e}")
+        return None
+
+
+def get_client_traffics(conn):
+    try:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM client_traffics")
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
+    except sqlite3.Error as e:
+        print(f"Ошибка выполнения запроса: {e}")
+
+
+def get_users(conn):
+    try:
+        cursor = conn.cursor()
+        cursor.execute("SELECT id, username FROM users")
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
+    except sqlite3.Error as e:
+        print(f"Ошибка выполнения запроса: {e}")
+
+
+def main():
+    db_path = DATABASE_FILE
+    conn = connect_to_db(db_path)
+
+    if conn:
+        get_client_traffics(conn)
+        get_users(conn)
+        conn.close()
+
+
+if __name__ == "__main__":
+    main()
